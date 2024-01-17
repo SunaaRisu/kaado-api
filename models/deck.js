768bbeaf3dd0bd1c4109ef2cc4d9d18b4ea0bcd0
Sunaa <sunaa@sunaarisu.de>
2024-01-17 13:28:08 +0100

    deck model changes

--- models/deck.js
+++ models/deck.js
@@ -1,13 +1,32 @@
 const mongoose = require('mongoose');
 
+// const deck = mongoose.Schema({
+//     title: {type: String},
+//     card_count: {type: Number},
+//     cards: {type: Array},
+//     chartDefinition: {
+//         chart_columns: {type: Number},
+//         chart_columns_name: {type: Array}
+//     }
+// })
+
 const deck = mongoose.Schema({
-    title: {type: String},
-    card_count: {type: Number},
+    deck_info: {
+        title: {type: String},
+        author: {type: String},
+        card_count: {type: Number},
+        chartDefinition: {
+            chart_columns: {type: Number},
+            chart_columns_name: {type: Array}
+        }
+    },
+    deck_settings: {
+        cards_per_stack: {type: Number},
+        card_layout: {type: String},
+        card_layout_filling: {type: String},
+        randomize: {type: Boolean}
+    },
     cards: {type: Array},
-    chartDefinition: {
-        chart_columns: {type: Number},
-        chart_columns_name: {type: Array}
-    }
 })
 
 module.exports = mongoose.model('Deck', deck);
